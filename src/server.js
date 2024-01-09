d4f5b0755183d28d427233f950b75fbfd70d6728
Sanjaya1717 <imaderikiwidiastanasanjaya@gmail.com>
2024-01-09 10:42:26 +0800

    local storage implemented

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
+const Inert = require('@hapi/inert');
+const path = require('path');
 
 // notes
 const notes = require('./api/notes');
@@ -30,6 +32,11 @@ const _exports = require('./api/exports');
 const ProducerService = require('./services/rabbitmq/ProducerService');
 const ExportsValidator = require('./validator/exports');
 
+// uploads
+const uploads = require('./api/uploads');
+const StorageService = require('./services/storage/StorageService');
+const UploadsValidator = require('./validator/uploads');
+
 // exceptions
 const ClientError = require('./exceptions/ClientError');
 const NotFoundError = require('./exceptions/NotFoundError');
@@ -41,6 +48,7 @@ const init = async () => {
   const notesService = new NotesService(collaborationsService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
+  const storageService = new StorageService(path.resolve(__dirname, 'api/uploads/file/images'));
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -53,7 +61,7 @@ const init = async () => {
   });
 
   // external plugin
-  await server.register([{ plugin: Jwt }]);
+  await server.register([{ plugin: Jwt }, { plugin: Inert }]);
 
   // mendefinisikan strategy autentikasi jwt
   server.auth.strategy('notesapp_jwt', 'jwt', {
@@ -110,6 +118,13 @@ const init = async () => {
       options: {
         service: ProducerService,
         validator: ExportsValidator,
+      },
+    },
+    {
+      plugin: uploads,
+      options: {
+        service: storageService,
+        validator: UploadsValidator,
       },
     },
   ]);
